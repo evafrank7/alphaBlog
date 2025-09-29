ba5cba19a9c3fb73d4c13e5777de7990137f9681
Eva Frankovic <emfrankovic7@gmail.com>
2025-09-29 11:21:40 -0400

    Articles Index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) # Params Hash contains the :id
   end
+
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+
 end
