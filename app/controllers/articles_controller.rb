4394c1fb8a6fd522179ba01b323af89d3e158f11
Eva Frankovic <emfrankovic7@gmail.com>
2025-09-29 14:28:34 -0400

    Adding more actions and buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,43 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id]) # Params Hash contains the :id
-  end
-
-  def index
-    @articles = Article.all
-  end
-  
-  def new
-    @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
- def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    if(@article.save)
-        flash[:notice] = "Article was successfully created" 
-        redirect_to article_path(@article)
-    else 
-        render :new
+    def show
+        @article = Article.find(params[:id]) # Params Hash contains the :id
     end
- end 
 
- def update
-    @article = Article.find(params[:id])
-    if(@article.update(params.require(:article).permit(:title, :description)))
-        flash[:notice] = "Article was successfully updated" 
-        redirect_to article_path(@article)
-    else 
-        render :edit
+    def index
+        @articles = Article.all
     end
- end 
+    
+    def new
+        @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if(@article.save)
+            flash[:notice] = "Article was successfully created" 
+            redirect_to article_path(@article)
+        else 
+            render :new
+        end
+    end 
+
+    def update
+        @article = Article.find(params[:id])
+        if(@article.update(params.require(:article).permit(:title, :description)))
+            flash[:notice] = "Article was successfully updated" 
+            redirect_to article_path(@article)
+        else 
+            render :edit
+        end
+    end 
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+      end
 end
