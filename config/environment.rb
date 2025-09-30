f29a7d329ddcd391981d1ea71538d82a45a29b25
Eva Frankovic <emfrankovic7@gmail.com>
2025-09-30 13:07:52 -0400

    Added styling for edit and new article functionality

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Custom error field rendering to avoid wrapping fields with errors in a div
+# This prevents breaking the layout when there are validation errors
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
