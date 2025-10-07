4f59231b209f5f9cf9945ee177baa5221e3b4319
Eva Frankovic <emfrankovic7@gmail.com>
2025-10-07 13:59:39 -0400

    Index View and Test for Categories

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "getting new category form and creating the category" do
+    get new_category_path # or "/categories/new"
+    assert_response :success # get request was successful
+
+    assert_difference "Category.count", 1 do
+      post categories_path, params: { category: { name: "Sports" } }
+      assert_response :redirect # we get redirected after post
+    end
+    follow_redirect! # follow the redirect to new page
+    assert_response :success # get request was successful
+    assert_match "Sports", response.body # check for success message
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " "} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
+end
